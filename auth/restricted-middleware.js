09d1d582fb5678af88e99471be94ab9b71e6b1d0
eric capiz <ericcapiz@gmail.com>
2020-12-07 20:38:23 -0600

    MVP

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -3,7 +3,7 @@ const secrets = require('../config/secrets');
 
 module.exports = (req,res,next)=>{
     const authheader = req.headers.authorization || '';
-    const token = authheader.split('')[1];
+    const token = authheader.split(' ')[1];
 
     if(token){
         jwt.verify(token,secrets.jwtSecret,(err,decodedToken)=>{
